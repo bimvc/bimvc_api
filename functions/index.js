11791d3fe1b413282b3217339ce77049f8c053bc
pltnm239 <pltnm239@gmail.com>
2018-05-05 12:55:23 +0300

    add cercts

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,24 @@ const admin = require('firebase-admin');
 const Mailchimp = require('mailchimp-api-v3');
 const mailchimp = new Mailchimp('08c0778e8d05d0a8d4545cb66973a4bd-us14');
 admin.initializeApp(functions.config().firebase);
+const pdf = require('html-pdf');
 
 exports.onNewUser = functions.auth.user().onCreate(user => {
     const { email } = user;
-    mailchimp.request({
-        method : 'post',
-        path : '/lists/07583bb04d/members/',
-        body : {
-            "email_address": email,
-            "status": "subscribed"
-        },
-    }, val => console.log('mailchimp', val))
+
+    return Promise.resolve()
+        .then(() => mailchimp.request({
+            method : 'post',
+            path : '/lists/07583bb04d/members/',
+            body : {
+                "email_address": email,
+                "status": "subscribed",
+                "merge_fields": {
+                    "FNAME": "Urist",
+                    "LNAME": "McVankab"
+                }
+            },
+        }, val => console.log('mailchimp', val)));
 });
 
 exports.letsTest = functions.https.onRequest((req, res) => {
@@ -37,4 +44,10 @@ exports.letsTest = functions.https.onRequest((req, res) => {
         })
         .then(() => res.sendStatus(200))
         .catch(e => e);
+});
+
+exports.pdf = functions.https.onRequest((req, res) => {
+    pdf.create('<h1>TEST</h1>').toFile('../public/cert.pdf', (err, res) => {
+        return Promise.resolve(err, res);
+    });
 });
